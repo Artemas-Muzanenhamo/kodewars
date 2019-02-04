4749d037c66d551fe3c6071087f142705d8dbd67
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-02-04 08:55:11 +0000

    amend Sum solution

--- src/main/kotlin/com/artemas/codewars/fundamentals/Sum.kt
+++ src/main/kotlin/com/artemas/codewars/fundamentals/Sum.kt
@@ -1,3 +1,4 @@
 package com.artemas.codewars.fundamentals
 
-fun sum(numbers: IntArray): Int = numbers.fold(0){ a, b -> a + (if (b < 0) 0 else b)}
+fun sum(numbers: IntArray): Int = numbers.filter { (it > 0) }.sum()
+//fun sum(numbers: IntArray): Int = numbers.fold(0){ a, b -> a + (if (b < 0) 0 else b)}
