f4c565355becfd13dbeee81b40fc9336b0ba60a7
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-02-04 15:36:09 +0000

    Change Sum to Kotlin Class

--- src/main/kotlin/com/artemas/codewars/fundamentals/Sum.kt
+++ src/main/kotlin/com/artemas/codewars/fundamentals/Sum.kt
@@ -1,4 +1,6 @@
 package com.artemas.codewars.fundamentals
 
-fun sum(numbers: IntArray): Int = numbers.filter { (it > 0) }.sum()
+class Sum {
+    fun sumPositives(numbers: IntArray): Int = numbers.filter { (it > 0) }.sum()
 //fun sum(numbers: IntArray): Int = numbers.fold(0){ a, b -> a + (if (b < 0) 0 else b)}
+}
--- src/test/kotlin/com/artemas/codewars/fundamentals/SumTest.kt
+++ src/test/kotlin/com/artemas/codewars/fundamentals/SumTest.kt
@@ -14,10 +14,11 @@ Note: if there is nothing to sum, the sum is default to 0.
 class SumTest {
     @Test
     fun testSomething() {
-        assertEquals(15, sum(intArrayOf(1, 2, 3, 4, 5)))
-        assertEquals(13, sum(intArrayOf(1, -2, 3, 4, 5)))
-        assertEquals(0, sum(intArrayOf()))
-        assertEquals(0, sum(intArrayOf(-1, -2, -3, -4, -5)))
-        assertEquals(9, sum(intArrayOf(-1, 2, 3, 4, -5)))
+        val sum = Sum()
+        assertEquals(15, sum.sumPositives(intArrayOf(1, 2, 3, 4, 5)))
+        assertEquals(13, sum.sumPositives(intArrayOf(1, -2, 3, 4, 5)))
+        assertEquals(0, sum.sumPositives(intArrayOf()))
+        assertEquals(0, sum.sumPositives(intArrayOf(-1, -2, -3, -4, -5)))
+        assertEquals(9, sum.sumPositives(intArrayOf(-1, 2, 3, 4, -5)))
     }
 }
