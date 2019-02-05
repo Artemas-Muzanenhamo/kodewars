86f3781e73adfced0850c8b79c384a2e4c1c5f74
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-02-05 09:36:35 +0000

    Add Opposite solution

---
+++ src/main/kotlin/com/artemas/codewars/fundamentals/Opposite.kt
@@ -0,0 +1,9 @@
+package com.artemas.codewars.fundamentals
+
+class Opposite {
+  companion object {
+    fun opposite(number: Int): Int {
+      return -number
+    }
+  }
+}
---
+++ src/test/kotlin/com/artemas/codewars/fundamentals/OppositeTests.kt
@@ -0,0 +1,17 @@
+package com.artemas.codewars.fundamentals
+
+import com.artemas.codewars.fundamentals.Opposite.Companion.opposite
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class OppositeTests {
+
+  /*Very simple, given a number, find its opposite.*/
+
+  @Test
+  fun testFixed() {
+    assertEquals(-1, opposite(1))
+    assertEquals(0, opposite(0))
+    assertEquals(25, opposite(-25))
+  }
+}
