e6a1c5e1461b47108ec2b117990815e7161a3738
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-02-07 20:41:49 +0000

    Add Functional Programming package

---
+++ src/main/kotlin/com/artemas/codewars/fundamentals/functional/FilterDemo.kt
@@ -0,0 +1,9 @@
+package com.artemas.codewars.fundamentals.functional
+
+class FilterDemo {
+    companion object {
+        fun sumPositives(listOfNumber: List<Int>): Int = listOfNumber.filter { it > 0 }.sum()
+        fun sumNegatives(listOfNumber: List<Int>): Int = listOfNumber.filter { it < 0 }.sum()
+//        fun sumPositivesSquared(listOfNumber: List<Int>) = listOfNumber.filter { it < 0 }.forEach {  }
+    }
+}
---
+++ src/test/kotlin/com/artemas/codewars/fundamentals/functional/FilterDemoTest.kt
@@ -0,0 +1,25 @@
+package com.artemas.codewars.fundamentals.functional
+
+import com.artemas.codewars.fundamentals.functional.FilterDemo.Companion.sumNegatives
+import com.artemas.codewars.fundamentals.functional.FilterDemo.Companion.sumPositives
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class FilterDemoTest {
+    private val listOfNumbers: List<Int> = listOf(20, -10, 33, 44, -2, -94, 22)
+
+    @Test
+    fun testSumPositives() {
+        assertEquals(119, sumPositives(listOfNumbers))
+    }
+
+    @Test
+    fun testSumNegatives() {
+        assertEquals(-106, sumNegatives(listOfNumbers))
+    }
+
+    @Test
+    fun testPositiveSumSquared() {
+        assertEquals(1000, sumPositivesSquared(listOfNumbers))
+    }
+}
