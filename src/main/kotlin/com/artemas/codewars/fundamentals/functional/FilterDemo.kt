e7ba4146d8e3e616b1588f7806098646f42944b4
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-02-07 20:42:06 +0000

    add TODO

--- src/main/kotlin/com/artemas/codewars/fundamentals/functional/FilterDemo.kt
+++ src/main/kotlin/com/artemas/codewars/fundamentals/functional/FilterDemo.kt
@@ -4,6 +4,7 @@ class FilterDemo {
     companion object {
         fun sumPositives(listOfNumber: List<Int>): Int = listOfNumber.filter { it > 0 }.sum()
         fun sumNegatives(listOfNumber: List<Int>): Int = listOfNumber.filter { it < 0 }.sum()
+        // TODO: Complete
 //        fun sumPositivesSquared(listOfNumber: List<Int>) = listOfNumber.filter { it < 0 }.forEach {  }
     }
 }
