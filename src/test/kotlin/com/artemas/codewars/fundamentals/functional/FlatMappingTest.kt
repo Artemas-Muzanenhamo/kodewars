72fc0545ed1a0ed55f9c4ff8af44e70e86d80cbe
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-02-09 09:34:32 +0000

    polish up

--- src/test/kotlin/com/artemas/codewars/fundamentals/functional/FlatMappingTest.kt
+++ src/test/kotlin/com/artemas/codewars/fundamentals/functional/FlatMappingTest.kt
@@ -6,9 +6,9 @@ import kotlin.test.assertEquals
 
 class FlatMappingTest{
 
-    val sublist1: List<Int> = listOf(10, 20, 30)
-    val sublist2: List<Int> = listOf(40, 50, 60)
-    val listOfNumbers: List<List<Int>> = listOf(sublist1, sublist2)
+    private val sublist1: List<Int> = listOf(10, 20, 30)
+    private val sublist2: List<Int> = listOf(40, 50, 60)
+    private val listOfNumbers: List<List<Int>> = listOf(sublist1, sublist2)
 
     @Test
     fun should_return_sum_of_all_values_in_sublists() {
