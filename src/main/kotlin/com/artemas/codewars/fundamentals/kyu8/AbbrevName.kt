078de5dd1a9033366d112d27cde0375793efedfb
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-02-09 21:23:18 +0000

    Add AbbrevName solution

---
+++ src/main/kotlin/com/artemas/codewars/fundamentals/kyu8/AbbrevName.kt
@@ -0,0 +1,7 @@
+package com.artemas.codewars.fundamentals.kyu8
+
+class AbbrevName {
+    companion object {
+        fun abbrevName(name: String): String = name.split(" ").map { it.take(1) }.joinToString(".")
+    }
+}
---
+++ src/test/kotlin/com/artemas/codewars/fundamentals/kyu8/AbbrevNameTest.kt
@@ -0,0 +1,17 @@
+package com.artemas.codewars.fundamentals.kyu8
+
+import com.artemas.codewars.fundamentals.kyu8.AbbrevName.Companion.abbrevName
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class AbbrevNameTest {
+
+    @Test
+    fun testNames() {
+        assertEquals(abbrevName("Sam Harris"), "S.H");
+        assertEquals(abbrevName("Patrick Feenan"), "P.F");
+        assertEquals(abbrevName("Evan Cole"), "E.C");
+        assertEquals(abbrevName("P Favuzzi"), "P.F");
+        assertEquals(abbrevName("David Mendieta"), "D.M");
+    }
+}
