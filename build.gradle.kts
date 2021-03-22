1bc06e77ea1b7e3db0ca11ceac81cd6657874b13
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2021-03-22 10:14:42 +0000

    upgrade gradle wrapper to version 6.6.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,10 +4,15 @@
  * This generated file contains a sample Kotlin library project to get you started.
  */
 
+//plugins {
+//    // Apply the Kotlin JVM plugin to add support for Kotlin on the JVM.
+//    id("org.jetbrains.kotlin.jvm").version("1.3.11")
+//}
+
 plugins {
-    // Apply the Kotlin JVM plugin to add support for Kotlin on the JVM.
-    id("org.jetbrains.kotlin.jvm").version("1.3.11")
+    kotlin("jvm") version "1.4.31"
 }
+
 
 repositories {
     // Use jcenter for resolving your dependencies.
