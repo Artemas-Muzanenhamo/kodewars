5c4272ade28d22315b1598d522aaa677dddd2b0a
Artemas-Muzanenhamo <artemasseng@gmail.com>
2023-05-14 10:10:40 +0100

    amend TheOfficeOuted Solution

--- src/main/kotlin/com/artemas/codewars/fundamentals/kyu7/TheOfficeOuted.kt
+++ src/main/kotlin/com/artemas/codewars/fundamentals/kyu7/TheOfficeOuted.kt
@@ -3,18 +3,7 @@ package com.artemas.codewars.fundamentals.kyu7
 class TheOfficeOuted {
     companion object {
         fun outed(meet: Map<String, Int>, boss: String): String {
-            val totalScore = meet.entries.map {
-                if (it.key == boss) return@map (it.value * 2) else it.value
-            }.sum()
-
-            val totalPeopleInTheRoom = meet.entries.count()
-            val happinessRating = totalScore / totalPeopleInTheRoom
-
-            return if (happinessRating <= 5) {
-                "Get Out Now!"
-            } else {
-                "Nice Work Champ!"
-            }
+            return if ((meet.values.sum() + meet.getOrDefault(boss, 0)) / meet.count() > 5) "Nice Work Champ!" else "Get Out Now!"
         }
     }
 }
